327c4f8bd20e9516b576f8605de05e2a4fc25555
Grant <grantdenbrock@icloud.com>
2024-07-05 16:21:23 -0700

    feat: also calls the python script

--- main.go
+++ main.go
@@ -11,4 +11,10 @@ func main () {
     log.Fatal(err)
   }
   fmt.Println(string(cmd))
+  
+  cmd2, err2 := exec.Command("python", "hello.py").Output()
+    if err2 != nil {
+      log.Fatal(err2)
+    }
+  fmt.Println(string(cmd2))
 }
